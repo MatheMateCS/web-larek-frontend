40b8f356dd8936151016305123d5cb5becc38ea1
Honsage <musatov.i.a@edu.mirea.ru>
2025-03-28 18:36:36 +0300

    feat(view): add abstract orderSuccess-page component

---
+++ src/components/view/OrderSuccess.ts
@@ -0,0 +1,19 @@
+import { ISuccess, SuccessInfo } from "../../types";
+import { ensureElement } from "../../utils/utils";
+import { Component } from "./Component";
+
+export abstract class Success extends Component<SuccessInfo> {
+    private __descriptionElement: HTMLParagraphElement;
+    private __closeButtonElement: HTMLButtonElement;
+
+    constructor(_container: HTMLElement, _handlers?: Partial<ISuccess>) {
+        super(_container);
+        this.__descriptionElement = ensureElement<HTMLParagraphElement>('.order-success__description', this.container);
+        this.__closeButtonElement = ensureElement<HTMLButtonElement>('.order-success__close', this.container);
+        this.__closeButtonElement.addEventListener('click', _handlers?.onClick);
+    }
+
+    set totalSum(totalSum: number) {
+        this.setText(this.__descriptionElement, `Списано ${totalSum} синапсов`);
+    }
+}
