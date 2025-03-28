03f36f13639682256534b43efbe8c40ad88c9091
Honsage <musatov.i.a@edu.mirea.ru>
2025-03-28 16:55:59 +0300

    feat(api): add interactor with server api

---
+++ src/components/api/ServerApi.ts
@@ -0,0 +1,34 @@
+import { Api } from '../base/api';
+import { API_URL, CDN_URL } from '../../utils/constants';
+import { ProductList, ProductItem, OrderInfo, Order } from '../../types/index';
+
+export class ServerApi extends Api {
+    constructor() {
+        super(API_URL);
+    }
+
+    _completeProductInfo(product: ProductItem): ProductItem {
+        return {
+            ...product,
+            image: `${CDN_URL}/${product.image}`,
+        };
+    }
+
+    async getProductList(): Promise<ProductList> {
+        const productList = (await this.get('/product')) as ProductList;
+        const products: ProductItem[] = productList.items.map(this._completeProductInfo);
+        return {
+            ...productList,
+            items: products,
+        };
+    }
+
+    async getProductItem(id: string): Promise<ProductItem> {
+        const product = (await this.get(`/product/${id}`)) as ProductItem;
+        return this._completeProductInfo(product);
+    }
+
+    postOrder(orderInfo: OrderInfo): Promise<Order> {
+        return this.post('/order', orderInfo) as Promise<Order>;
+    }
+}
