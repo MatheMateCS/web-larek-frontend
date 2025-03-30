240a4d97b2172086613adb5c264b0e2993ab3243
Honsage <musatov.i.a@edu.mirea.ru>
2025-03-31 01:50:05 +0300

    feat(view): add abstract modal-window component

---
+++ src/components/view/Modal.ts
@@ -0,0 +1,41 @@
+import { IModal, ModalInfo } from "../../types";
+import { ensureElement } from "../../utils/utils";
+import { Component } from "./Component";
+
+export abstract class Modal extends Component<ModalInfo> {
+    protected _closeButtonElement: HTMLButtonElement;
+    protected _content: HTMLElement;
+
+    constructor(container: HTMLElement, 
+                protected _handlers?: Partial<IModal>) {
+        super(container);
+        
+        this._closeButtonElement = ensureElement<HTMLButtonElement>('.modal__close', container);
+        this._content = ensureElement<HTMLElement>('.modal__content', container);
+
+        this._closeButtonElement.addEventListener('click', this.close.bind(this));
+        this.container.addEventListener('click', this.close.bind(this));
+        this._content.addEventListener('click', evt => evt.stopPropagation());
+    }
+
+    set content(content: HTMLElement) {
+        this._content.replaceChildren(content);
+    }
+
+    public open() {
+        this.container.classList.add('modal_active');
+        this._handlers?.onOpen();
+    }
+
+    public close() {
+        this.container.classList.remove('modal_active');
+        this._content = null;
+        this._handlers?.onClose();
+    }
+
+    render(content: ModalInfo): HTMLElement {
+        super.render(content);
+        this.open();
+        return this.container;
+    }
+}
