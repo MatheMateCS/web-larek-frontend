44e320d023e280d6d67110a5b1c653ef7ab0ed1b
Honsage <musatov.i.a@edu.mirea.ru>
2025-03-31 22:42:24 +0300

    feat(view): add implementation of basket-item-bar component

---
+++ src/components/view/BasketItemBar.ts
@@ -0,0 +1,19 @@
+import { IEvents, ProductItem } from "../../types";
+import { cloneTemplate } from "../../utils/utils";
+import { BasketItem } from "./BasketItem";
+
+export class BasketItemBar extends BasketItem<ProductItem> {
+    constructor(events: IEvents) {
+        super(cloneTemplate<HTMLLIElement>('#card-basket'), {
+            onRemove: () => events.emit('basketItem:remove', { id: this.id })
+        });
+    }
+
+    set id(id: string) {
+        this.container.dataset.id = id || '';
+    }
+
+    get id() {
+        return this.container.dataset.id || '';
+    }
+}
