614ec1fd12839a3053aa8b158ed2da76902bedde
Honsage <musatov.i.a@edu.mirea.ru>
2025-03-31 20:05:14 +0300

    feat(view): add implementation of modal-window component

---
+++ src/components/view/ModalWindow.ts
@@ -0,0 +1,12 @@
+import { IEvents } from "../../types";
+import { ensureElement } from "../../utils/utils";
+import { Modal } from "./Modal";
+
+export class ModalWindow extends Modal {
+    constructor(events: IEvents) {
+        super(ensureElement<HTMLDivElement>('#modal-container'), {
+            onOpen: () => events.emit('modal:open'),
+            onClose: () => events.emit('modal:close')
+        });
+    }
+}
