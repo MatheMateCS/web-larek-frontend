39fea57df8f435cbe18c1c719d856b972909e124
Honsage <musatov.i.a@edu.mirea.ru>
2025-03-31 21:09:26 +0300

    feat(view): add implementation of order-modal component

---
+++ src/components/view/OrderModal.ts
@@ -0,0 +1,32 @@
+import { IEvents, OrderInfo } from "../../types";
+import { cloneTemplate, ensureElement } from "../../utils/utils";
+import { Form } from "./Form";
+
+export class OrderModal extends Form<OrderInfo> {
+    private __onlineTypeButtonElement: HTMLButtonElement;
+    private __cashTypeButtonElement: HTMLButtonElement;
+
+    constructor(events: IEvents) {
+        super(cloneTemplate<HTMLFormElement>('#order'), {
+            onInput: (field, value, message) => events.emit('order:change', { address: value }),
+            onSubmit: () => events.emit('order:submit')
+        });
+
+        this.__onlineTypeButtonElement = ensureElement<HTMLButtonElement>('button[name=card]', this.container);
+        this.__cashTypeButtonElement = ensureElement<HTMLButtonElement>('button[name=cash]', this.container);
+
+        this.toggleClass(this.__onlineTypeButtonElement, 'button_alt-active');
+
+        this.__onlineTypeButtonElement.addEventListener('click', () => {
+            this.toggleClass(this.__onlineTypeButtonElement, 'button_alt-active', true);
+            this.toggleClass(this.__cashTypeButtonElement, 'button_alt-active', false);
+            events.emit('orderInfo:change', { payment: 'Онлайн' });
+        });
+
+        this.__cashTypeButtonElement.addEventListener('click', () => {
+            this.toggleClass(this.__cashTypeButtonElement, 'button_alt-active', true);
+            this.toggleClass(this.__onlineTypeButtonElement, 'button_alt-active', false);
+            events.emit('orderInfo:change', { payment: 'При получении' });
+        });
+    }
+}
